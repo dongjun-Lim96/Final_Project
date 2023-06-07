450e293b136229840239aff909a34e0124c089e0
seongjiwoong <tjdwldnd111@naver.com>
2023-06-07 14:51:28 +0900

    seongjiwoong<tjdwldnd111@naver.com>

--- Final_Project/src/main/java/course/model/CourseDao.java
+++ Final_Project/src/main/java/course/model/CourseDao.java
@@ -7,7 +7,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import product.model.ProductBean;
+
 
 @Component
 public class CourseDao {
