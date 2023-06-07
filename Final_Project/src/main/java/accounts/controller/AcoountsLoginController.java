11cd6c3de438d53eb34a4f0e51aa2eb37693d230
seongjiwoong <tjdwldnd111@naver.com>
2023-06-07 15:04:36 +0900

    seongjiwoong<tjdwldnd111@naver.com>

--- Final_Project/src/main/java/accounts/controller/AcoountsLoginController.java
+++ Final_Project/src/main/java/accounts/controller/AcoountsLoginController.java
@@ -77,7 +77,7 @@ public class AcoountsLoginController {
 				// "redirect:/detail.prd" : 주문하기  
 				
 				
-				mav.setViewName("redirect:/main.jsp");
+				mav.setViewName("redirect:/list.cs");
 				
 			}else { // 가입은 했는데 비번이 일치하지 않는다.
 				
