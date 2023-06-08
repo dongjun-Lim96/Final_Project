dc17e85c5002d5f68d3948a6b7ea092caad8f98b
seongjiwoong <tjdwldnd111@naver.com>
2023-06-08 11:19:13 +0900

    r

--- Final_Project/src/main/java/category/model/CategoryBean.java
+++ Final_Project/src/main/java/category/model/CategoryBean.java
@@ -1,9 +1,28 @@
 package category.model;
 
-
-
 public class CategoryBean {
 	private String categoryCode;
 	private String categoryName;
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	public CategoryBean() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public CategoryBean(String categoryCode, String categoryName) {
+		super();
+		this.categoryCode = categoryCode;
+		this.categoryName = categoryName;
+	}
 	
 }
