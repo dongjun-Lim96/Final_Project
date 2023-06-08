ce749ba478e8020bb0e82c3b7ee05e49ed4bea54
seongjiwoong <tjdwldnd111@naver.com>
2023-06-08 17:49:20 +0900

    courseDao - jiwoong

--- Final_Project/src/main/java/course/model/CourseDao.java
+++ Final_Project/src/main/java/course/model/CourseDao.java
@@ -24,6 +24,16 @@ public class CourseDao {
 		return lists;
 	}
 
+	public boolean searchCode(String courseCode) {
+		boolean result = false; 
+		int cnt = sqlSessionTemplate.selectOne(namespace+".SearchCgCode", courseCode);
+		if (cnt>0) {
+			result = true;
+		}
+		
+		return result;
+	}
+
 	
 	
 }
