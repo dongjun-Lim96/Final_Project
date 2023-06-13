9dc6c4d386d8b93a14ff286618c5b34e902a3521
seongjiwoong <tjdwldnd111@naver.com>
2023-06-13 17:11:54 +0900

    ㅇ

--- Final_Project/src/main/java/course/controller/CourseGetMainController.java
+++ Final_Project/src/main/java/course/controller/CourseGetMainController.java
@@ -1,8 +1,13 @@
 package course.controller;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +25,22 @@ public class CourseGetMainController {
 	
 	@Autowired
 	CourseDao coursedao;
+
+	@Autowired
+	ServletContext servletContext;	
 	
 	@RequestMapping(value = command)
-	public ModelAndView doAction() {
+	public ModelAndView doAction() throws IOException {
+		
+		String uploadPath = servletContext.getRealPath("/resources/");
+		System.out.println("uploadPath: " + uploadPath);
+		File destination = new File(uploadPath);
+		
+		String str = "C:/tempUpload"; 
+		File destination_local = new File(str); //폴더로 만듬
+		
+		FileUtils.copyDirectory(destination_local, destination); //폴더쨰 카피
+		
 		System.out.println("/list.cs/list.cs/list.cs");
 		ModelAndView mav = new ModelAndView();
 		
