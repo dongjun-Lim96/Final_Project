42ba59b1a3c4e4354111d00062a807d9ef06543d
minjejoo <joominje@gmail.com>
2023-06-13 17:25:57 +0900

    dd

--- Final_Project/src/main/java/course/controller/CourseGetMainController.java
+++ Final_Project/src/main/java/course/controller/CourseGetMainController.java
@@ -1,8 +1,13 @@
 package course.controller;
 
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +20,34 @@ import course.model.CourseDao;
 
 @Controller
 public class CourseGetMainController {
-	private final String command="/list.cs";
-	private final String getPage="main";
-	
-	@Autowired
-	CourseDao coursedao;
-	
-	@RequestMapping(value = command)
-	public ModelAndView doAction() {
-		System.out.println("/list.cs/list.cs/list.cs");
-		ModelAndView mav = new ModelAndView();
-		
-		List<CourseBean> courseLists = coursedao.getCourse();
-		mav.addObject("courseLists",courseLists);
-		mav.setViewName(getPage);
-		return mav; 
-	}
-	 
+   private final String command="/list.cs";
+   private final String getPage="main";
+   
+   @Autowired
+   CourseDao coursedao;
+
+   @Autowired
+   ServletContext servletContext;   
+   
+   @RequestMapping(value = command)
+   public ModelAndView doAction() throws IOException {
+      
+      String uploadPath = servletContext.getRealPath("/resources/");
+      System.out.println("uploadPath: " + uploadPath); 
+      File destination = new File(uploadPath);
+      
+      String str = "C:/tempUpload"; 
+      File destination_local = new File(str); //폴더로 만듬
+      
+      FileUtils.copyDirectory(destination_local, destination); //폴더쨰 카피
+      
+      System.out.println("/list.cs/list.cs/list.cs");
+      ModelAndView mav = new ModelAndView();
+      
+      List<CourseBean> courseLists = coursedao.getCourse();
+      mav.addObject("courseLists",courseLists);
+      mav.setViewName(getPage);
+      return mav; 
+   }
+    
 }
