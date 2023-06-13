a6fd5e5e755af0b13fc6c0968836ef6ab9bb8144
machineWook <jweun908@naver.com>
2023-06-13 18:06:09 +0900

    j

---
+++ Final_Project/src/main/java/course/controller/CoursePaidController.java
@@ -0,0 +1,50 @@
+package course.controller;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.ex.MyBean;
+
+import course.model.CourseBean;
+import course.model.CourseDao;
+
+@Controller
+public class CoursePaidController { 
+	private final String command="/paidCourse.cs";
+	private final String getPage="CoursePaid";
+	
+	@Autowired 
+	CourseDao courseDao;
+	
+	@Autowired
+	ServletContext servletContext;
+	
+	@RequestMapping(value = command)
+	public ModelAndView doAction() throws IOException {
+		
+		
+		String uploadPath = servletContext.getRealPath("/resources/");
+		System.out.println("uploadPath: " + uploadPath);
+		File destination = new File(uploadPath);
+		
+		String str = "C:/tempUpload"; 
+		File destination_local = new File(str);
+		
+		FileUtils.copyDirectory(destination_local, destination);
+
+		ModelAndView mav = new ModelAndView();
+		List<CourseBean> paidCourseLists= courseDao.getPaidCourse();
+		mav.addObject("paidCourseLists", paidCourseLists);
+		mav.setViewName(getPage);
+		return mav;
+	}
+}
