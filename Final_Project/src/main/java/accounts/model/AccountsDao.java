880d6566eabda7082676ecaebdf0edba8695018f
minjejoo <joominje@gmail.com>
2023-06-21 22:53:47 +0900

    sssss

--- Final_Project/src/main/java/accounts/model/AccountsDao.java
+++ Final_Project/src/main/java/accounts/model/AccountsDao.java
@@ -35,107 +35,107 @@ import java.util.List;
 @Component
 public class AccountsDao {
 
-	private String namespace = "accounts.model";
-	
-	@Autowired
-	SqlSessionTemplate sqlSessionTemplate;
+   private String namespace = "accounts.model";
+   
+   @Autowired
+   SqlSessionTemplate sqlSessionTemplate;
 
-	
-	public AccountsBean GetAccountsById(String userId) {
-		AccountsBean ab= new AccountsBean();
-		ab = sqlSessionTemplate.selectOne(namespace+".GetAccountsById",userId); 
-		return ab;
+   
+   public AccountsBean GetAccountsById(String userId) {
+      AccountsBean ab= new AccountsBean();
+      ab = sqlSessionTemplate.selectOne(namespace+".GetAccountsById",userId); 
+      return ab;
 
-	}
-
-
-	public AccountsBean GetUserIdByUserEmail(String input_userEmail) {
-		AccountsBean ab= new AccountsBean();
-		ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserEmail",input_userEmail); 
-		
-		return ab;
-	}
+   }
 
 
-	public AccountsBean GetAccountsByName(String input_userName) {
-		AccountsBean ab= new AccountsBean();
-		ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserName",input_userName); 
-		
-		return ab;
-	}
-
-	public AccountsBean GetAccountsByEmail(String input_userEmail) {
-		AccountsBean ab= new AccountsBean();
-		ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserEmail",input_userEmail); 
-		
-		return ab;
-	}
-
-	public AccountsBean GetAccountsByNameEmail(String input_userName, String input_userEmail) {
-		AccountsBean ab= new AccountsBean();
-		
-		HashMap<String, String> M =new HashMap<String, String>(); 
-		M.put("userName", input_userName);
-		M.put("userEmail", input_userEmail);
-		ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserNameEmail",M); 
-		
-		return ab;
-	}
+   public AccountsBean GetUserIdByUserEmail(String input_userEmail) {
+      AccountsBean ab= new AccountsBean();
+      ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserEmail",input_userEmail); 
+      
+      return ab;
+   }
 
 
-	public AccountsBean GetAccountsByIdEmail(String input_userId, String input_userEmail) {
-		AccountsBean ab= new AccountsBean();
-		HashMap<String, String> M =new HashMap<String, String>(); 
-		M.put("userId", input_userId);
-		M.put("userEmail", input_userEmail);
-		ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserIdEmail",M); 
-		
-		return ab;
-	}
-	
-	public boolean searchId(String userId) {
-		boolean result = false; 
-		int cnt = sqlSessionTemplate.selectOne(namespace+".SearchId", userId);
-		if (cnt>0) {
-			result = true;
-		}
-		
-		return result;
-	}
-	
-	public int accountsRegister(AccountsBean ab) {
-		int cnt = -1;
-		cnt = sqlSessionTemplate.insert(namespace+".AccountsRegister", ab);
-		
-		return cnt;
-	}
-	
-	
-	public List<CourseBean> getOrderById(String userId) {
-		List<CourseBean> lists = new ArrayList<CourseBean>();
-		lists = sqlSessionTemplate.selectList(namespace+".GetOrderById",userId); 
-		return lists;
-	}
-	
-	public int accountsUpdate(AccountsBean ab) {
-		int cnt = -1;
-		cnt = sqlSessionTemplate.update(namespace+".AccountsUpdate", ab);
-		
-		return cnt;
-	}
-	
-	public void outUser(String userId) {
-		int cnt = -1;
-		cnt = sqlSessionTemplate.update(namespace+".OutUser", userId);
-	}
-	
-	public List<CourseBean> getWishList(String userId) {
-		List<CourseBean> lists = new ArrayList<CourseBean>();
-		lists = sqlSessionTemplate.selectList(namespace+".GetWishList", userId); 
-		return lists;
-	}
+   public AccountsBean GetAccountsByName(String input_userName) {
+      AccountsBean ab= new AccountsBean();
+      ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserName",input_userName); 
+      
+      return ab;
+   }
 
-	  public List<CourseOrderBean> getPayList(String userId) {
+   public AccountsBean GetAccountsByEmail(String input_userEmail) {
+      AccountsBean ab= new AccountsBean();
+      ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserEmail",input_userEmail); 
+      
+      return ab;
+   }
+
+   public AccountsBean GetAccountsByNameEmail(String input_userName, String input_userEmail) {
+      AccountsBean ab= new AccountsBean();
+      
+      HashMap<String, String> M =new HashMap<String, String>(); 
+      M.put("userName", input_userName);
+      M.put("userEmail", input_userEmail);
+      ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserNameEmail",M); 
+      
+      return ab;
+   }
+
+
+   public AccountsBean GetAccountsByIdEmail(String input_userId, String input_userEmail) {
+      AccountsBean ab= new AccountsBean();
+      HashMap<String, String> M =new HashMap<String, String>(); 
+      M.put("userId", input_userId);
+      M.put("userEmail", input_userEmail);
+      ab = sqlSessionTemplate.selectOne(namespace+".GetUserIdByUserIdEmail",M); 
+      
+      return ab;
+   }
+   
+   public boolean searchId(String userId) {
+      boolean result = false; 
+      int cnt = sqlSessionTemplate.selectOne(namespace+".SearchId", userId);
+      if (cnt>0) {
+         result = true;
+      }
+      
+      return result;
+   }
+   
+   public int accountsRegister(AccountsBean ab) {
+      int cnt = -1;
+      cnt = sqlSessionTemplate.insert(namespace+".AccountsRegister", ab);
+      
+      return cnt;
+   }
+   
+   
+   public List<CourseBean> getOrderById(String userId) {
+      List<CourseBean> lists = new ArrayList<CourseBean>();
+      lists = sqlSessionTemplate.selectList(namespace+".GetOrderById",userId); 
+      return lists;
+   }
+   
+   public int accountsUpdate(AccountsBean ab) {
+      int cnt = -1;
+      cnt = sqlSessionTemplate.update(namespace+".AccountsUpdate", ab);
+      
+      return cnt;
+   }
+   
+   public void outUser(String userId) {
+      int cnt = -1;
+      cnt = sqlSessionTemplate.update(namespace+".OutUser", userId);
+   }
+   
+   public List<CourseBean> getWishList(String userId) {
+      List<CourseBean> lists = new ArrayList<CourseBean>();
+      lists = sqlSessionTemplate.selectList(namespace+".GetWishList", userId); 
+      return lists;
+   }
+
+   public List<CourseOrderBean> getPayList(String userId) {
 	      List<CourseOrderBean> lists = new ArrayList<CourseOrderBean>();
 	      lists = sqlSessionTemplate.selectList(namespace+".GetPayList", userId);
 	      return lists;
@@ -153,7 +153,7 @@ public class AccountsDao {
 	      lists = sqlSessionTemplate.selectList(namespace+".GetCourseEndList", userId); 
 	      return lists;
 	   }
-	public String getAccessToken (String authorize_code) {
+   public String getAccessToken (String authorize_code) {
         String access_Token = "";
         String refresh_Token = "";
         String reqURL = "https://kauth.kakao.com/oauth/token";
@@ -167,7 +167,7 @@ public class AccountsDao {
             conn.setRequestMethod("POST");
             conn.setDoOutput(true);
 
-            //	POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
+            //   POST 요청에 필요로 요구하는 파라미터 스트림을 통해 전송
             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
             StringBuilder sb = new StringBuilder();
             sb.append("grant_type=authorization_code");
@@ -210,10 +210,10 @@ public class AccountsDao {
 
         return access_Token;
     }
-	 
-	 
-	public HashMap<String, Object> getUserInfo (String access_Token) {
-			System.out.println("access_Token:::::"+access_Token);
+    
+    
+   public HashMap<String, Object> getUserInfo (String access_Token) {
+         System.out.println("access_Token:::::"+access_Token);
         //    요청하는 클라이언트마다 가진 정보가 다를 수 있기에 HashMap타입으로 선언
         HashMap<String, Object> userInfo = new HashMap<String, Object>();
         String reqURL = "https://kapi.kakao.com/v2/user/me";
@@ -266,14 +266,14 @@ public class AccountsDao {
     }
 
 
-	public AccountsBean GetAccountsEmail(String kakao_email) {
-		AccountsBean ab= new AccountsBean();
-		ab = sqlSessionTemplate.selectOne(namespace+".GetAccountsEmail",kakao_email); 
-		
-		return ab;
-	}
-	 
-	 
-	 
-	
+   public AccountsBean GetAccountsEmail(String kakao_email) {
+      AccountsBean ab= new AccountsBean();
+      ab = sqlSessionTemplate.selectOne(namespace+".GetAccountsEmail",kakao_email); 
+      
+      return ab;
+   }
+      
+    
+    
+   
 }
